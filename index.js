f58fca1ad9efa76a9366fd86d0789ec9115718ae
winoysoydigdig <46377807+winoysoydigdig@users.noreply.github.com>
2019-05-06 10:41:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("youtube", {type: "WATCHING"});
+  bot.user.setActivity("winoysoydigdig", {type: "WATCHING"});
 
 });
 
